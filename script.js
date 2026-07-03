d758d4d80cb0fcac189dc67c90f189cf0714aa30
zoyyaab-pixel <zoyyaa.b@gmail.com>
2026-07-04 00:38:43 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const envelope = document.getElementById("envelope");
+const page1 = document.getElementById("page1");
+const page2 = document.getElementById("page2");
+
+envelope.addEventListener("click", () => {
+    page1.style.display = "none";
+    page2.style.display = "flex";
+});
