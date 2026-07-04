ea6c96fba6a5b5810f13f592b9a13eec0ae192eb
zoyyaab-pixel <zoyyaa.b@gmail.com>
2026-07-04 18:16:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-alert("Script Loaded!");
-
 const envelope = document.getElementById("envelope");
 const page1 = document.getElementById("page1");
 const page2 = document.getElementById("page2");
+const music = document.getElementById("music");
 
 envelope.addEventListener("click", () => {
     page1.style.display = "none";
     page2.style.display = "flex";
-    document.getElementById("music").play();
+
+    if (music) {
+        music.play().catch(err => {
+            console.log("Music couldn't play:", err);
+        });
+    }
 });
