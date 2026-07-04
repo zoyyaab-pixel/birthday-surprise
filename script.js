a798aeead295c364c27a3473212f23d8a262e6e7
zoyyaab-pixel <zoyyaa.b@gmail.com>
2026-07-04 16:46:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const envelope = document.getElementById("envelope");
 const page1 = document.getElementById("page1");
 const page2 = document.getElementById("page2");
+const music = document.getElementById("music");
 
 envelope.addEventListener("click", () => {
     page1.style.display = "none";
     page2.style.display = "flex";
+
+    music.play();
 });
